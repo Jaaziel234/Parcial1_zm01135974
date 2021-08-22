bfd145a142315d3d0febaf9025b03036a30945da
jaaziel234 <jaazielmc45@gmail.com>
2021-08-21 20:10:45 -0600

    primer commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading text-center">EsauCode</div>
+
+                <div class="panel-body text-center">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Bienvenidos ya te encuentras registrado
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
